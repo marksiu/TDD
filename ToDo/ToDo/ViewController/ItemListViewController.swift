14aff2c551421d469e0b60972261ae7dc0c3f636
Mark Siu <mark.siu@gtomato.com>
2016-12-22 09:35:53 +0800

    1. Deleted ViewController due to useless for system generate 2. Updated storyboard to add the ItemListViewController and tableview 3. Add the test to ItemListViewControllerTests

---
+++ ToDo/ToDo/ViewController/ItemListViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ItemListViewController.swift
+//  ToDo
+//
+//  Created by Mark Siu on 16/10/2016.
+//  Copyright © 2016 Mark Siu. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+
+    @IBOutlet var tableView: UITableView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        tableView = UITableView()
+    }
+    
+    
+    
+
+
+}
