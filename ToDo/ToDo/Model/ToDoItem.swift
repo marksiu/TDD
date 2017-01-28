22d3003239268d8319f634f6845debe27397dcfa
Mark Siu <mark.siu@gtomato.com>
2017-01-28 22:35:05 +0800

    Ch 3 P.68 - P.76 1. Confirm Equatable protocol in ToDoItem 2. Add tests to ToDoItemTest 3. Confirm Equatable protocol in Location 4. Implement common function in LocationTests to perform the test

--- ToDo/ToDo/Model/ToDoItem.swift
+++ ToDo/ToDo/Model/ToDoItem.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct ToDoItem {
+struct ToDoItem: Equatable {
     let title: String
     let itemDescription: String?
     let timestamp: Double?
@@ -19,5 +19,24 @@ struct ToDoItem {
         self.itemDescription = itemDescription
         self.timestamp = timestamp
         self.location = location
+    }
+    
+    static func ==(lhs: ToDoItem, rhs: ToDoItem) -> Bool {
+        if lhs.title != rhs.title {
+            return false
+        }
+        
+        if lhs.itemDescription != rhs.itemDescription {
+            return false
+        }
+        
+        if lhs.timestamp != rhs.timestamp {
+            return false
+        }
+        
+        if lhs.location?.name != rhs.location?.name {
+            return false
+        }
+        return true
     }
 }
